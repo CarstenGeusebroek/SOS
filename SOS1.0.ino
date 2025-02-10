6521d58ff96a5d79cdadf14367bc9b90b9b4012c
Carsten <83281084+CarstenGeusebroek@users.noreply.github.com>
2025-02-10 17:32:21 +0100

    Commentaar regels toegevoegd

--- SOS1.0.ino
+++ SOS1.0.ino
@@ -1,3 +1,4 @@
+// Include de eerder genoemde library's voor het maken van een bluetooth connectie met de controller
 #include <PS4BT.h>
 #include <usbhub.h>
 
@@ -6,20 +7,25 @@
 #endif
 #include <SPI.h>
 
+// Het maken van instances van de USB verbinding en daarme de bluetooth instance
 USB Usb;
-
 BTD Btd(&Usb);
 
+// Vervolgens wordt de PS4 bluetooth connectie gemaakt door het verwijzen van de bluetooth instance in de contstructor van de PS4BT class
 PS4BT PS4(&Btd, PAIR);
 
 void setup()
 {
+  // Zorgt ervoor dat pin 3 als output kan worden gebruikt later
   pinMode(3, OUTPUT);
 
+  // Debug code voor het kunnen outputten van teksten in de console.
   Serial.begin(115200);
 #if !defined(__MIPSEL__)
   while (!Serial)
 #endif
+
+    // Check of de Usb class juist is gemaakt en die vervolgens initaliseren
     if (Usb.Init() == -1)
     {
       Serial.print(F("\r\nOSC did not start"));
@@ -30,16 +36,16 @@ void setup()
 
   void loop()
   {
+    // Functie van de Usb class die wordt gebruikt om alle functies te callen binnen de Usb class die elke keer nodig zijn.
     Usb.Task();
+
     if (PS4.connected())
     {
+      // Output een PWM signaal naar de eerder benoemde output pin. Analog write accepteerd waarden tussen de 0 en 255 en de getAnalogButton functie output waarden tussen de 0 en 255 dus dit komt perfect uit.
       analogWrite(3, PS4.getAnalogButton(R2));
     }
 
+    // Laat de controller fysiek trillen, dit zorgt voor het makelijk debuggen van de code.
     PS4.setRumbleOn(PS4.getAnalogButton(L2), PS4.getAnalogButton(R2));
-  }
-  else
-  {
-    analogWrite(3, 0);
   }
 }
